6c522566d1cde77a00141fc2339e60683f3715b6
BazingaTesla <deniz.guvel@gmail.com>
2024-06-12 00:49:11 +0300

    "Edit Profile Screen" was created
    -Added profile image
    -Added fields on Firebase
    -Created details_card file
    -image_ picker

--- lib/views/profile_screen/profile_screen.dart
+++ lib/views/profile_screen/profile_screen.dart
@@ -1,8 +1,10 @@
 import 'package:emart_app/consts/consts.dart';
 import 'package:emart_app/consts/lists.dart';
 import 'package:emart_app/controllers/auth_controller.dart';
+import 'package:emart_app/controllers/profile_controller.dart';
 import 'package:emart_app/views/auth_screen/login_screen.dart';
 import 'package:emart_app/views/profile_screen/components/details_card.dart';
+import 'package:emart_app/views/profile_screen/edit_profile_screen.dart';
 import 'package:emart_app/widgets_common/bg_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,6 +14,9 @@ class ProfileScreen extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
+
+    var controller = Get.put(ProfileController());
+
     return bgWidget(
       child: Scaffold(
         body: SafeArea(
@@ -26,7 +31,9 @@ class ProfileScreen extends StatelessWidget{
                   child: Icon(
                     Icons.edit,
                     color: whiteColor,),
-                  ).onTap(() {}),
+                  ).onTap(() {
+                    Get.to(() => const EditProfileScreen());
+                  }),
                 ),
 
                 //Users details section
