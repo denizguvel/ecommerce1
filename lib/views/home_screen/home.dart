52157b59057067d11ab5f1a10b1783876f37d1b1
BazingaTesla <deniz.guvel@gmail.com>
2024-06-18 23:35:06 +0300

    Added exit dialog

--- lib/views/home_screen/home.dart
+++ lib/views/home_screen/home.dart
@@ -7,6 +7,7 @@ import 'package:emart_app/views/cart_screen/cart_screen.dart';
 import 'package:emart_app/views/category_screen/category_screen.dart';
 import 'package:emart_app/views/home_screen/home_screen.dart';
 import 'package:emart_app/views/profile_screen/profile_screen.dart';
+import 'package:emart_app/widgets_common/exit_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -31,21 +32,30 @@ class Home extends StatelessWidget {
       const CartScreen(),
       const ProfileScreen()
     ];
-    return Scaffold(
-      body: Column(children: [ 
-         Obx(()=> Expanded(child: navBody.elementAt(controller.currentNavIndex.value))),
-      ],), 
-      bottomNavigationBar: Obx(()=>
-      BottomNavigationBar(
-        currentIndex: controller.currentNavIndex.value,
-        selectedItemColor: redColor,
-        selectedLabelStyle: const TextStyle(fontFamily: semibold),
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: whiteColor,
-        items: navbarItem,
-        onTap: (value){
-          controller.currentNavIndex.value = value;
-        }),
+    return WillPopScope(
+      onWillPop: () async{
+        showDialog(
+          barrierDismissible: false,
+          context: context, 
+          builder: (context) => exitDialog(context));
+        return false;
+      },
+      child: Scaffold(
+        body: Column(children: [ 
+          Obx(()=> Expanded(child: navBody.elementAt(controller.currentNavIndex.value))),
+        ],), 
+        bottomNavigationBar: Obx(()=>
+        BottomNavigationBar(
+          currentIndex: controller.currentNavIndex.value,
+          selectedItemColor: redColor,
+          selectedLabelStyle: const TextStyle(fontFamily: semibold),
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: whiteColor,
+          items: navbarItem,
+          onTap: (value){
+            controller.currentNavIndex.value = value;
+          }),
+        )
       )
     );
   }
